45714127a705a59c55f51f50535b133c44100944
J-Mara <jmaragakis20@stuy.edu>
2020-12-13 22:26:45 -0500

    final version

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -18,6 +18,12 @@ public class NoNullArrayList<T> extends ArrayList<T>{
       super.add(index, element);
     }
   }
+  public T set(int index, T element){
+    if(element == null){
+      throw new IllegalArgumentException("value is null");
+    }
+    return super.set(index, element);
+  }
   public NoNullArrayList(){
     super();
   }
